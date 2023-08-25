9639ff93a8a8a0a251a58f19c42a938fba4fd533
SuperNeon4ik <youtube@superneon4ik.me>
2023-08-25 23:34:50 +0300

    Add an anime girl

--- injector.js
+++ injector.js
@@ -1,7 +1,13 @@
+console.log("meow meow lol");
+
 const style = document.createElement('link');
 style.rel = "stylesheet";
 style.href = chrome.runtime.getURL("kawaii-theme.css");
 (document.head || document.documentElement).appendChild(style);
-document.documentElement.classList.add("kawaii");
 
-console.log("meow meow lol")
+const animeGirl = document.createElement("img");
+animeGirl.src = chrome.runtime.getURL("images/anime-girl-1.png");
+animeGirl.classList.add("animegirl");
+(document.body || document.documentElement).appendChild(animeGirl);
+
+document.documentElement.classList.add("kawaii");
