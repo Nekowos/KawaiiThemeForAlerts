f5c9fffc12442ed760bdf4c71c3f327fbbbc0657
SuperNeon4ik <youtube@superneon4ik.me>
2023-08-25 23:22:19 +0300

    Initial themes (light, dark)

--- injector.js
+++ injector.js
@@ -1,7 +1,7 @@
 const style = document.createElement('link');
 style.rel = "stylesheet";
-style.href = chrome.runtime.getURL("kawaii-light.css");
+style.href = chrome.runtime.getURL("kawaii-theme.css");
 (document.head || document.documentElement).appendChild(style);
-document.documentElement.classList.add("kawaii-light");
+document.documentElement.classList.add("kawaii");
 
 console.log("meow meow lol")
